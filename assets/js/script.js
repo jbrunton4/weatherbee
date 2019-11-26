c0161c4fab5cd749e64ed24876f894bc633f8847
Josh Brunton <39703189+jbrunton4@users.noreply.github.com>
2019-11-26 18:25:16 +0000

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const here = window.location.href;
+
+function SocialFacebook() {
+  // Link to official facebook page
+}
+
+function SocialTwitter() {
+  // Link to official instagram page
+}
+
+function SocialTwitter() {
+  // Link to official twitter page
+}
+
+function Redir(subfolder) {
+  window.location.href += $"/{subfolder}";
+}
